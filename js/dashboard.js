23ac71f2ea10019389b78874e990fd089eddb497
LaiseBang <lbangh@yahoo.com>
2018-12-16 12:22:03 +0100

    Graphs changes according to count

--- js/dashboard.js
+++ js/dashboard.js
@@ -73,9 +73,11 @@ function searchItems(items) {
             shavingCount += 1;
         }
 
+        dogGraphs();
+        clothesGraphs();
+        toiletriesGraphs();
     });
     console.log("hundemad: " + dogFoodCount + " hundetøj: " + dogClothesCount + " hundelegetøj: " + dogToyCount + " Frakke:" + wintercoatCount + " Vanter: " + hatAndGlovesCount + " Sokker: " + socksCount + " Tandbørste: " + toothbrushCount + " Tamponer: " + tamponsCount + " Barbergrej: " + shavingCount);
-    dogGraphs();
 }
 
 function errData(err) {
@@ -84,10 +86,31 @@ function errData(err) {
 }
 
 function dogGraphs() {
-    let height = (dogFoodCount * 1000);
-    console.log(height);
-    // let Ypos = 217 - height;
-    document.querySelector("#doggraph1").setAttribute("height", height + "px");
-    //  clone.querySelector("#Layer_2 rect.cls-1").setAttribute("y", Ypos);
+    let dogFoodHeight = (dogFoodCount * 10);
+    let dogClothesHeight = (dogClothesCount * 10);
+    let dogToyHeight = (dogToyCount * 10);
+    document.querySelector("#doggraph1").setAttribute("style", "height:" + dogFoodHeight + "px");
+    document.querySelector("#doggraph2").setAttribute("style", "height:" + dogClothesHeight + "px");
+    document.querySelector("#doggraph3").setAttribute("style", "height:" + dogToyHeight + "px");
+    console.log(dogFoodHeight, +" " + dogClothesHeight + " " + dogToyHeight);
 }
 
+function clothesGraphs() {
+    let wintercoatHeight = (wintercoatCount * 10);
+    let hatAndGlovesHeight = (hatAndGlovesCount * 10);
+    let socksHeight = (socksCount * 10);
+    document.querySelector("#clothesgraph1").setAttribute("style", "height:" + wintercoatHeight + "px");
+    document.querySelector("#clothesgraph2").setAttribute("style", "height:" + hatAndGlovesHeight + "px");
+    document.querySelector("#clothesgraph3").setAttribute("style", "height:" + socksHeight + "px");
+    console.log(wintercoatHeight, +" " + hatAndGlovesHeight + " " + socksHeight);
+}
+
+function toiletriesGraphs() {
+    let toothbrushHeight = (toothbrushCount * 10);
+    let tamponsHeight = (tamponsCount * 10);
+    let shavingHeight = (shavingCount * 10);
+    document.querySelector("#toiletriesgraph1").setAttribute("style", "height:" + toothbrushHeight + "px");
+    document.querySelector("#toiletriesgraph2").setAttribute("style", "height:" + tamponsHeight + "px");
+    document.querySelector("#toiletriesgraph3").setAttribute("style", "height:" + shavingHeight + "px");
+    console.log(toothbrushHeight, +" " + tamponsHeight + " " + shavingHeight);
+}
