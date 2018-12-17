020cd36e0d81b0a0aba55f3cb4faf48f56234d58
Marie Dyrmose <mari48v8@stud.kea.dk>
2018-12-17 10:57:12 +0100

    first commit corrections2

--- js/index.js
+++ js/index.js
@@ -10,17 +10,3 @@ function myShowFunction() {
     }
     //document.getElementById("panel").style.display = "block";
 }
-
-//show form/ opretkonto.html when btnSignUp is clicked
-btnSignUp.addEventListener('click', e => {
-    console.log("button find")
-    document.location.href = 'pages/opretkonto.html';
-
-})
-
-//show form/ opretkonto.html when btnSignUp is clicked
-btnLogin.addEventListener('click', e => {
-    console.log("button find")
-    document.location.href = 'pages/loginform.html';
-
-})
---
+++ js/onclick.js
@@ -0,0 +1,20 @@
+//show form/ opretkonto.html when btnSignUp is clicked
+btnSignUp.addEventListener('click', e => {
+    console.log("button find")
+    document.location.href = 'pages/opretkonto.html';
+
+})
+
+//show form/ opretkonto.html when btnSignUp is clicked
+btnLogin.addEventListener('click', e => {
+    console.log("button find")
+    document.location.href = 'pages/loginform.html';
+
+})
+
+//show form/ opretkonto.html when btnSignUp is clicked
+btnForgot.addEventListener('click', e => {
+    console.log("button find")
+    document.location.href = 'pages/glemkode2.html';
+
+})
