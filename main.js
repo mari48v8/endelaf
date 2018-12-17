26a3bb9ddc3407f8603536e7fde5bef321bc7176
LaiseBang <lbangh@yahoo.com>
2018-12-17 11:46:10 +0100

    hide and show login, new user and logout buttons

--- main.js
+++ main.js
@@ -1,55 +1,29 @@
 'use strict';
 
 // Get elements
-//const txt_email = document.getElementById("txt_email");
-//const txt_password = document.getElementById("txt_password");
-
-// button that opens the form
 const btnLogin = document.getElementById("btnLogin");
 const btnLogout = document.getElementById("btnLogout");
-// // button that send the form
-//const btnSubmitLogIn = document.getElementById("btnSubmitLogIn");
-//const btnSubmitSignUp = document.getElementById("btnSubmitSignUp");
-
-// container used to hide/ show the login form
-//const loginForm = document.getElementById("container-login");
-//const btnSignUp = document.getElementById("btnSignUp");
-
+const btnSignUp = document.getElementById("btnSignUp");
 
 let email;
 let pass;
 let auth;
 let promise;
 
-
 //Add realtime listner
 firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
-        console.log(firebaseUser);
-        //Log Out
         btnLogout.classList.remove('hide');
         btnLogin.classList.add('hide');
+        btnSignUp.classList.add('hide');
 
-
-
-        //Signed in
-        //  btnSignUp.classList.add('hide');
-        document.querySelector("#uId").textContent = firebaseUser.uid;
-        document.querySelector("#user").textContent = firebaseUser.email;
         // Add userId to local storage
         localStorage.setItem("uId", firebaseUser.uid);
 
-        // Retrieve - @TODO hvor skal denne placeres?????
-        // document.getElementById("uId2").innerHTML = sessionStorage.getItem("uId");
     } else {
         console.log("not logged in");
         btnLogout.classList.add('hide');
-        //reaload page
-
+        btnLogin.classList.remove('hide');
+        btnSignUp.classList.remove('hide');
     }
 })
-
-// //Eventlistner submitSignUp button
-// btnSignUp.addEventListener('click', e => {
-//     console.log("sendt");
-// }) */
