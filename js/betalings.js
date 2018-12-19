9eedf75ae3ae41968ce91e269de4ab62eded0eda
Marie Dyrmose <mari48v8@stud.kea.dk>
2018-12-19 02:04:55 +0100

    donation styling og javascript done

---
+++ js/betalings.js
@@ -0,0 +1,29 @@
+"use strict"
+
+function btnRadio(x) {
+    if (x == 0) {
+        document.getElementById("btnYes").style.display = "block";
+        document.getElementById("swip").style.display = "none";
+    } else {
+        document.getElementById("btnYes").style.display = "none";
+        document.getElementById("swip").style.display = "block";
+        return;
+    }
+}
+
+function removeC(x) {
+    let element = document.getElementById("mobile-pay-confirm");
+    element.classList.remove("not-active");
+}
+
+function show1() {
+    document.getElementById('div1').style.display = 'block';
+    document.getElementById('div2').style.display = 'none';
+    //element.classList.add("not-active");
+}
+
+function show2() {
+    document.getElementById('div1').style.display = 'none';
+    document.getElementById('div2').style.display = 'block';
+
+}
