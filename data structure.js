39da179f2b6248ad743c710c2eba796d2d0ac0ca
LaiseBang <lbangh@yahoo.com>
2018-12-19 17:38:11 +0100

    rename data structure.js

---
+++ data structure.js
@@ -0,0 +1,33 @@
+// an array of all donations, every object has an item
+donations[
+    {
+        item: [
+            {
+                name: "dog",
+                id: "dogToy",
+                value: "100",
+            },
+            {
+                name: "clothes",
+                id: "wintercoat",
+                value: "200",
+            }
+        ],
+        uID: "xxx", //userId from localStorage
+        createdAt: "2018-12-03T15:24:52.770Z"
+    }
+]
+
+// Users
+users[
+    {
+        name: "Marie",
+        lastname: "Dyrmose",
+        address: "gade 1",
+        zip: "1424",
+        city: "Kbh",
+        email: "100",
+        mobile: "22 22 22 22",
+        uId: "xxxxxxxxxxxxxxx",
+    }
+]
